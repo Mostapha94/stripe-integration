d24c0f808d923e8b81350804bd5dd0334ce1b7a7
Mostapha94 <m.sayed@medexsepeti.com>
2024-09-10 14:27:19 +0200

    create migration for user payment cards

---
+++ database/migrations/2024_09_10_012731_create_user_payment_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_payment_cards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('card_number');
+            $table->string('card_holder_name');
+            $table->string('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_payment_cards');
+    }
+};
